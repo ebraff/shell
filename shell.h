467405a776df341bc772bd621b5802473218952b
Jesse Huang <jeshuang@eden.rutgers.edu>
2014-02-20 15:37:14 -0500

    added builtin struct to header

--- shell.h
+++ shell.h
@@ -11,6 +11,13 @@ typedef struct command
 	struct command *next;
 } command;
 
+typedef struct builtins 
+{
+     char *name;	/* name of function */
+     int (*f)();	/* function to execute for the built-in command */
+} builtin;
+
+
 command *parse(char *input);
 void freeCmd(command *cmd);
 void printCmd(command *cmd);
